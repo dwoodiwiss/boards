1ca8612d8e63432c34230cfe0d45fc87fd7fbec1
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2015-02-19 22:18:19 +0000

    add some strictly typed data to understand data binding

---
+++ client/boards.js
@@ -0,0 +1,33 @@
+if (Meteor.isClient) {
+  // counter starts at 0
+  Session.setDefault('counter', 0);
+
+  Template.hello.helpers({
+    counter: function() {
+      return Session.get('counter');
+    }
+  });
+
+  Template.board.helpers({
+    boards: function() {
+      var boards = [
+        {
+          title: "Board Title",
+          description: "This is where the content for the board will go."
+        },
+        {
+          title: "Another Board Title",
+          description: "This is where the content for the board will go, it might even be quite long content... WHO KNOWS!"
+        }
+      ]
+      return boards;
+    }
+  });
+
+  Template.hello.events({
+    'click button': function() {
+      // increment the counter when button is clicked
+      Session.set('counter', Session.get('counter') + 1);
+    }
+  });
+}
