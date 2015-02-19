19a7dd02deaee30bd92c874a31e94716c4c8051d
Darren Woodiwiss <darrenwoodiwiss@gmail.com>
2015-02-19 22:43:24 +0000

    add $primary-color, style button, style nav, comment out meteor starter code

--- client/boards.js
+++ client/boards.js
@@ -1,12 +1,12 @@
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
+  // Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function() {
-      return Session.get('counter');
-    }
-  });
+  // Template.hello.helpers({
+  //   counter: function() {
+  //     return Session.get('counter');
+  //   }
+  // });
 
   Template.board.helpers({
     boards: function() {
@@ -24,10 +24,10 @@ if (Meteor.isClient) {
     }
   });
 
-  Template.hello.events({
-    'click button': function() {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+  // Template.hello.events({
+  //   'click button': function() {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
 }
